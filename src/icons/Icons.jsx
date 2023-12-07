ac3cf533705cfc42be6afaa51b6e72ba8231594c
diego072117 <parracalderond9@gmail.com>
2023-12-07 16:46:19 -0500

    fix icons

--- src/icons/Icons.jsx
+++ src/icons/Icons.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const IconMenu = () => (
   <svg width="40" height="17" xmlns="http://www.w3.org/2000/svg">
-    <g fill="#00001A" fill-rule="evenodd">
+    <g fill="#00001A">
       <path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z" />
       <path d="M0 0h40v3H0z" />
     </g>
@@ -11,7 +11,7 @@ export const IconMenu = () => (
 
 export const IconMenuClose = () => (
   <svg width="32" height="31" xmlns="http://www.w3.org/2000/svg">
-    <g fill="#00001A" fill-rule="evenodd">
+    <g fill="#00001A">
       <path d="m2.919.297 28.284 28.284-2.122 2.122L.797 2.419z" />
       <path d="M.797 28.581 29.081.297l2.122 2.122L2.919 30.703z" />
     </g>
